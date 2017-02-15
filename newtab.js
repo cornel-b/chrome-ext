041c1e6b5eedc0ee4263d562765feb4adb59ed08
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-02-15 23:38:18 +0000

    bookmarks

--- newtab.js
+++ newtab.js
@@ -6,3 +6,10 @@ chrome.topSites.get(function(sites) {
         $("#most-visited").append('<li><a href="' + element.url + '"><span class="tab"><img src="https://www.google.com/s2/favicons?domain=' + element.url + '" width="20" /></span></a></li>');
     }, this);
 });
+
+chrome.bookmarks.search('a', function(sites) {
+    sites = sites.slice(0, 12);
+    sites.forEach(function(element) {
+        $("#bookmarks").append('<li><a href="' + element.url + '"><span class="tab"><img src="https://www.google.com/s2/favicons?domain=' + element.url + '" width="20" /></span></a></li>');
+    }, this);
+});
