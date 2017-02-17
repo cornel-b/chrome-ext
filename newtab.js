2e88905ae498705fb979b247263f7191243030a9
Cornel <cornel.bicuti@gmail.com>
2017-02-17 13:38:36 +0000

    Update newtab.js

--- newtab.js
+++ newtab.js
@@ -7,23 +7,24 @@ chrome.topSites.get(function(sites) {
     }, this);
 });
 
+// display booksmarks
 chrome.bookmarks.search('a', function(sites) {
     sites = sites.slice(0, 12);
-    sites.forEach(function(element) {
-        $("#bookmarks").append('<li><a href="' + element.url + '"><span class="tab"><img src="https://www.google.com/s2/favicons?domain=' + element.url + '" width="20" /></span></a></li>');
-    }, this);
+    var list = sites.map(function(site) {
+        return '<li><a href="' + site.url + '"><span class="tab"><img src="https://www.google.com/s2/favicons?domain=' + site.url + '" width="20" /></span></a></li>';
+    });
+    
+    list.forEach(function(element) { $("#bookmarks").append(element) }, this);
 });
 
-
+// display tabs
 chrome.tabs.getAllInWindow(null, function(tabs) {
-
     tabs.forEach(function(tab) {
-        $("#tab-list").append('<li><a href="' + tab.url + '"><span class="tab">' + tab.title + '</span></a> <span class="close-tab" data-id="' + tab.index + '">x</span></li>');
+        $("#tab-list").append('<li><a href="' + tab.url + '"><span class="tab">' + tab.title + '</span></a> <span class="close-tab" data-id="' + tab.id + '">x</span></li>');
     }, this);
-
 });
 
-$(".close-tab").on('click', function() {
+$(document).on('click', '.close-tab', function() {
     var tabId = $(this).data('id');
     chrome.tabs.remove(tabId);
 });
