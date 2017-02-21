a177481e5e5b79ae333c6a9e426f7d73f5c6e185
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-02-21 22:13:30 +0000

    current time

--- newtab.js
+++ newtab.js
@@ -34,3 +34,26 @@ $(document).on('click', '.switch-tab', function() {
     var tabId = $(this).parent().data('id');
     chrome.tabs.update(tabId, {active: true});
 });
+
+
+// current time
+function updateTime()
+{
+    var date = new Date();
+    var hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+    var minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    $('#current-time').html(hour + ':' + minute);
+}
+
+$(function() {
+    updateTime();
+});
+
+function timeout() {
+    setTimeout(function () {
+        updateTime();
+        timeout();
+    }, 1000);
+}
+
+timeout();
