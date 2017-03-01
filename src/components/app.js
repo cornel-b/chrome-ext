acaf9c7cb71bf99ef271365a957d4a46b0bed0c0
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-03-01 23:01:55 +0000

    switch to react

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import MostVisited from './most-visited';
+
+export default class App extends Component {
+  render() {
+    return (
+      <MostVisited />
+    );
+  }
+}
---
+++ src/components/most-visited.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class MostVisited extends Component {
+  render() {
+    return (
+      <div>Most visited websites</div>
+    );
+  }
+}
