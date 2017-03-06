96a71daf49dc6b9f8563524124e2da447dc5a9c9
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-03-06 23:19:33 +0000

    Site item

--- src/components/site_list_item.js
+++ src/components/site_list_item.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
-export default class SiteListItem extends Component {
-  render() {
-    return (
-      <li><a href="{props.site.url}"><span class="tab"><img src="https://www.google.com/s2/favicons?domain={props.site.url}" width="20" /></span></a></li>
-    );
-  }
+const SiteListItem = ({site}) => {
+  return <li><a href="{site.url}"><span class="tab"><img src="https://www.google.com/s2/favicons?domain={site.url}" width="20" /></span></a></li>
 }
+
+export default SiteListItem;
