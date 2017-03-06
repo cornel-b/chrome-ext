9916bf187e5eff64c7d17c17024cc0a0cb4c2d77
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-03-06 23:20:41 +0000

    Site item

--- src/components/most-visited.js
+++ src/components/most-visited.js
@@ -3,11 +3,32 @@ import React, { Component } from 'react';
 import SiteListItem from './site_list_item'
 
 
-export default class MostVisited extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>Most visited websites</div>
-    );
-  }
+const MostVisited = (props) => {
+  console.log(props.sites);
+
+/* chrome.topSites.get(function(sites) {
+    sites = sites.slice(0, 12);
+    sites.forEach(function(element) {
+        $("#most-visited").append('<li><a href="' + element.url + '"><span class="tab"><img src="https://www.google.com/s2/favicons?domain=' + element.url + '" width="20" /></span></a></li>');
+    }, this);
+});*/
+
+  const sites = props.sites.get(function(sites) {
+    sites.forEach(function(element) {
+      console.log(element);
+      return <SiteListItem site={element} />
+    }, this);
+  });
+
+  return sites;
+
+  /*const siteItems = sites.map((site) => {
+    return <SiteListItem site={site} />
+  });*/
+
+   /*return (
+      <div>!!Most visited websites!!</div>
+    );*/
 }
+
+export default MostVisited;
