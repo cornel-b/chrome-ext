855af1f9f7d7164528e833b68e639c01fea295b4
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-03-07 23:14:14 +0000

    Most visited component

--- src/components/most-visited.js
+++ src/components/most-visited.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
 import SiteListItem from './site_list_item'
 
+class MostVisited extends React.Component {
 
-export default class MostVisited extends Component {
+  getSites() {
+      const sites = this.props.sites.get(function(sites) {
+        var list = sites.map(function(site)  {
+          return <SiteListItem site={site} />
+        });
+      });
+  };
+
   render() {
-
     return (
       <div>Most visited websites</div>
     );
   }
+
 }
+
+export default MostVisited;
