43a680d91a9705a0c1ee7e0a282910493588ae57
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-03-16 01:32:00 +0000

    Timer component

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { MostVisited } from './most_visited';
 import { Bookmarks } from './bookmarks';
+import { Timer } from './timer';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-       <MostVisited sites={this.props.chrome.topSites} />
-       <Bookmarks sites={this.props.chrome.bookmarks} />
+          <MostVisited sites={this.props.chrome.topSites} />
+          <Bookmarks sites={this.props.chrome.bookmarks} />
+          <Timer seconds="1500" />
       </div>
     );
   }
---
+++ src/components/timer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class Timer extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { time: 0, seconds: 0 };
+        this.timeLeft = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ time: this.state.seconds, seconds: this.props.seconds });
+    }
+
+    startTimer() {
+        if (this.timeLeft == 0) {
+            this.timeLeft = setInterval(this.countDown, 1000);
+        }
+    }
+
+    formatSeconds(seconds) {
+        let min = Math.floor(seconds / 60);
+        let sec = seconds - (min * 60);
+        return ((min > 0) ? min + "m " : "") + sec + "s";
+    }
+
+    countDown() {
+        let seconds = this.state.seconds - 1;
+        this.setState({
+            time: seconds,
+            seconds: seconds,
+        });
+
+        if (seconds == 0) {
+            clearInterval(this.timeLeft);
+        }
+    }
+
+    render() {
+        return (
+            <div className="timer">
+                <button className="timerButton" onClick={this.startTimer}>Go</button>
+                <div>{this.formatSeconds(this.state.time)}</div>
+            </div>
+        )
+    }
+
+}
