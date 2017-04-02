fbe65906a3de4be5de90a62da538dafdc66a23fe
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-04-02 20:37:06 +0100

    Do not use jquery for timer

--- newtab.js
+++ newtab.js
@@ -1,15 +1,10 @@
-// current time
 function updateTime()
 {
     var date = new Date();
     var hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
     var minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-    $('#current-time').html(hour + ':' + minute);
+    document.getElementById('current-time').innerHTML = hour + ':' + minute;
 }
-
-$(function() {
-    updateTime();
-});
 
 function timeout() {
     setTimeout(function () {
